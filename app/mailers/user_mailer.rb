c2a7ec36577cdc5f607a84d8548e9fb62b36e3c8
Mohit <mohitdutta77@gmail.com>
2014-10-31 18:56:14 +0530

    URL modification

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: 'contact@fundsinn.com'
+  default from: 'contact@abcd.com'
  
   def welcome_email(user,subject,message,from)
     @user = user
@@ -7,7 +7,7 @@ class UserMailer < ActionMailer::Base
     @message=message
     @from=from
     @code=Template.last.text_code
-    @url  = 'http://fundsinn.com'
+    @url  = 'http://mohit.com'
     mail(to: @user.email_address, subject: @subject)
   end
 end
